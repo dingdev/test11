d0b69e72374d0ea177667bc3563504cbb5e28e06
丁云鹏 <2798732983@qq.com>
2021-11-24 11:33:43 +0800

    all init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}---</h1>
+  <h3 class="text-blue-400">3级标题</h3>
+  <ol>
+    <li class="bg-red-200">aaa</li>
+    <li class="underline-yellow-400">bbb</li>
+    <li>ccc</li>
+  </ol>
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ windi.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "windicss/helpers";
+import colors from "windicss/colors";
+import plugin from "windicss/plugin"
+
+export default defineConfig({
+ darkMode: "media",
+ theme: {
+     extend: {
+         colors:{
+             gray: colors.coolGray
+         }
+     }
+
+ }
+
+})
